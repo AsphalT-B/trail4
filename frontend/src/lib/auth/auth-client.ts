af6bd806d8d281a901f24bc1e75acb2e45ad5775
AsphalT-B <asphaltinc329@gmail.com>
2026-09-15 12:18:08 +0100

    frontend edit2

--- frontend/src/lib/auth/auth-client.ts
+++ frontend/src/lib/auth/auth-client.ts
@@ -4,4 +4,11 @@ import { inferAdditionalFields } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
+  plugins: [
+    inferAdditionalFields({
+      user: {
+        role: { type: "string", input: false },
+      },
+    }),
+  ],
 });
