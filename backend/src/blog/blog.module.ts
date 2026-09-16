802c52df7d46d366fa177674936ad388eda2bf96
Daniel Ojerahi <ojerahidaniel89@gmail.com>
2026-09-16 17:55:02 +0100

    blog feature added3

---
+++ backend/src/blog/blog.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import {BlogService} from './blog.service.js'
+import { BlogController } from './blog.controller.js';
+
+@Module({
+  controllers: [BlogController],
+  providers: [BlogService],
+})
+export class BlogModule {}
